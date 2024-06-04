ccd21ca316cd549dd9b74cb1d999f88736294412
Kacper Mandziuk <kacper.mandziuk@monterail.com>
2024-06-04 12:00:02 +0200

    feat: add projects section

---
+++ components/project-card.js
@@ -0,0 +1,21 @@
+export default {
+  props: {
+    project: Object
+  },
+  template: /*html*/ `
+    <div class="project-card">
+       <div class="project-card__project-preview">
+            <img :src="'/assets/images/' + project.name + '.jpg'" alt="project screenshot">
+       </div>
+       <div class="project-card__project-info">
+            <h3>{{ project.name }}</h3>
+            <p>{{ project.description }}</p>
+            <div class="project-card__links">
+                <a :href="project.url">Live</a>
+                <a :href="project.github_repository">Github</a>
+            </div>
+       </div>
+    </div>
+  `
+}
+ 
