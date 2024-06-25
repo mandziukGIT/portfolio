985e8e9dc40100a6664a66aa3c724335ef98c447
Kacper Mandziuk <kacper.mandziuk@monterail.com>
2024-06-25 17:26:37 +0200

    feat: responsive about section + swiper js

--- components/experience-record.js
+++ components/experience-record.js
@@ -1,32 +1,36 @@
-import { computed } from "vue";
+import { computed, onMounted } from "vue";
 
 export default {
   props: {
     record: Object
   },
-  setup(props) {
+  emits: ['mounted'],
+  setup(props, ctx) {
     const experience = computed(() => props.record)
+
+    onMounted(() => {
+      ctx.emit('mounted')
+    })
 
     return { experience }
   },
   template: /*html*/ `
     <div class="about__experience-record">
-        <div class="about__record-left-col"> 
-            <div class="about__record-badges-list">
-                <span v-for="badge in record.badges">{{ badge }}</span>
-            </div> 
-            <p>TECHNOLOGIES:</p>           
-            <div class="about__record-tech-list">
-                <span v-for="tech in record.tech">{{ tech }}</span> 
-            </div>  
+      <div class="about__record-left-col"> 
+        <div class="about__record-logo">
+          <img :src="'assets/images/' + record.company.toLowerCase() + '-logo.svg'" alt="">
         </div>
-        <div class="about__record-middle-col">
-            <span>{{ experience.employment_start}} - {{ experience.employment_finish}}</span>
+        <div class="about__record-badges-list">
+            <span v-for="badge in record.badges">{{ badge }}</span>
         </div>
-        <div class="about__record-right-col">
-            <span class="about__record-position">{{ experience.position }}</span>
-            <p>{{ experience.description }}</p>
-        </div>
+      </div>
+      <div class="about__record-middle-col">
+        <span>{{ experience.employment_start}} - {{ experience.employment_finish}}</span>
+      </div>
+      <div class="about__record-right-col">
+        <span class="about__record-position">{{ experience.position }}</span>
+        <p>{{ experience.description }}</p>
+      </div>
     </div>
   `
 }
